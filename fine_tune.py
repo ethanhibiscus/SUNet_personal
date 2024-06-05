4b1cff4d5c6d1a2ca3c5221138d2ea04321082e7
hibiscusethan <ethanhsu@stanford.edu>
2024-06-05 16:37:47 -0700

    all images

--- fine_tune.py
+++ fine_tune.py
@@ -20,22 +20,6 @@ class GrayscaleImageDataset(Dataset):
 
     def __getitem__(self, idx):
         noisy_img_path = os.path.join(self.noisy_dir, self.noisy_images[idx])
-        reference_img_path 
-
-
-class GrayscaleImageDataset(Dataset):
-    def __init__(self, noisy_dir, reference_dir, transform=None):
-        self.noisy_dir = noisy_dir
-        self.reference_dir = reference_dir
-        self.noisy_images = sorted([img for img in os.listdir(noisy_dir) if img.endswith('.png')])
-        self.reference_images = sorted([img for img in os.listdir(reference_dir) if img.endswith('.png')])
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.noisy_images)
-
-    def __getitem__(self, idx):
-        noisy_img_path = os.path.join(self.noisy_dir, self.noisy_images[idx])
         reference_img_path = os.path.join(self.reference_dir, self.reference_images[idx])
         
         noisy_img = Image.open(noisy_img_path).convert('L')
